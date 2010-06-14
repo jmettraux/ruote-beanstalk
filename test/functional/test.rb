5f35918bff1f2e8e5e5d947680892a6d1da75d41
John Mettraux <jmettraux@gmail.com>
2010-06-14 21:30:32 +0900

    some test infra

---
+++ test/functional/test.rb
@@ -0,0 +1,9 @@
+
+#
+# testing ruote-beanstalk
+#
+# Mon Jun 14 21:28:47 JST 2010
+#
+
+Dir.glob(File.join(File.dirname(__FILE__), 'ft_*.rb')).sort.each { |t| load(t) }
+
---
+++ test/test.rb
@@ -0,0 +1,9 @@
+
+#
+# testing ruote-beanstalk
+#
+# Mon Jun 14 21:29:35 JST 2010
+#
+
+require File.join(File.dirname(__FILE__), 'functional', 'test')
+
