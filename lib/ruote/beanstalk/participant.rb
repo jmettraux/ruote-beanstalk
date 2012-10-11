13e624c490fb2d66d67e6f0a58de89fe6dba06c6
John Mettraux <jmettraux@gmail.com>
2012-10-11 11:11:36 +0900

    bring back Ruote::Beanstalk::Participant

--- lib/ruote/beanstalk/participant.rb
+++ lib/ruote/beanstalk/participant.rb
@@ -1,3 +1,12 @@
 
 require 'ruote/beanstalk/participant_proxy'
 
+
+#
+# The "Proxy" prefix is meant to indicate that the real action is happening
+# on the other side [of Beanstalk], but some people might prefer simply
+# writing "Ruote::Beanstalk::Participant".
+#
+class Ruote::Beanstalk::Participant < Ruote::Beanstalk::ParticipantProxy
+end
+
